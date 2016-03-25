ec48da2ca75ebffadffb87e499b7312420335d37
pbmolini <pbmolini@fbk.eu>
2016-03-25 14:35:37 +0100

    environmentalized db

--- lib/database_connector.rb
+++ lib/database_connector.rb
@@ -6,7 +6,8 @@ class DatabaseConnector
     def establish_connection
       ActiveRecord::Base.logger = Logger.new(active_record_logger_path)
 
-      configuration = YAML::load(IO.read(database_config_path))
+      # configuration = YAML::load(IO.read(database_config_path))
+      configuration = db_config
 
       ActiveRecord::Base.establish_connection(configuration)
     end
@@ -19,6 +20,10 @@ class DatabaseConnector
 
     def database_config_path
       'config/database.yml'
+    end
+
+    def db_config
+      {"adapter"=>"sqlite3", "database"=>ENV['DATABASE'], "encoding"=>"unicode", "pool"=>5, "timeout"=>5000}
     end
   end
 end
