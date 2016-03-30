8e1a2c4d4a85e3566b35cf265f2b0961c6e43e08
pbmolini <pbmolini@fbk.eu>
2016-03-30 15:59:39 +0200

    new workflow with enriched UI

--- lib/message_responder.rb
+++ lib/message_responder.rb
@@ -23,44 +23,37 @@ class MessageResponder
   def respond
 
     case @user.state
+    when 'ordering'
+      on /(Pizze|Insalate|Cucina)/ do |category|
+        @user.update_attributes state: "choosing_#{category.downcase.to_s}"
+        answer_with_menu category.downcase.to_sym
+      end
     when /choosing_[#{MenuItem::CATEGORIES.join('|')}]/
       on /Indietro/ do
+        go_to_start_menu
+      end
+
+      on /Menu/ do
         answer_with_menu @user.state.gsub('choosing_','')
       end
 
-      on /Ordina (.+)/ do |name|
+      on /^Ordina (.+)/ do |name|
         choose_meal_and_notify_pietro name
         @user.update_attributes state: nil
       end
 
-      on /^(?!.*(Indietro|Ordina)).*$/ do
-        answer_with_details
+      on /^(?!.*(Indietro|Ordina|Menu)).*$/ do
+         answer_with_details
       end
-
-      # choose_meal_and_notify_pietro message.text
-      # @user.update_attributes state: nil
     else
-      on /^\/(pizze|cucina|insalate)\s*$/ do |category|
-        @user.update_attributes state: "choosing_#{category.to_s}"
-        answer_with_menu category.to_sym
-      end
-
-      # For th Pros
-      on /^\/pizza (.+)/ do |pizza|
-        choose_meal_and_notify_pietro "\u{1f355} #{pizza.gsub(/[Pp]izza /, "")}"
-      end
-
-      on /^\/cucina (.+)/ do |cucina|
-        choose_meal_and_notify_pietro "\u{1f372} #{cucina}"
-      end
-
-      on /^\/insalata (.+)/ do |salad|
-        choose_meal_and_notify_pietro "\u{1f331} #{salad.gsub(/[Ii]nsalata /, "")}"
+      on /^\/start/ do
+        go_to_start_menu
       end
 
       on /^\/help/ do
         display_help
       end
+
     end
 
   end
@@ -81,10 +74,26 @@ class MessageResponder
 
   end
 
+  def go_to_start_menu
+    answer_with_start_menu
+    @user.update_attributes state: "ordering"
+  end
+
+  def answer_with_start_menu
+    MessageSender.new(bot: bot,
+                      chat: message.chat,
+                      text: "Pizza, insalata o qualcosa dalla cucina?",
+                      answers: [
+                        "#{MenuBuilder.emoji_for(:pizze)} Pizze",
+                        "#{MenuBuilder.emoji_for(:insalate)} Insalate",
+                        "#{MenuBuilder.emoji_for(:cucina)} Cucina"
+                      ]).send
+  end
+
   def answer_with_menu category
     items = MenuBuilder.retrieve(category)
     names = items.map(&:name)
-    names.any? ? display_keyboard_for(names) : answer_with_orostube_closed
+    names.any? ? display_keyboard_for(names + ["\u{25c0} Indietro"]) : answer_with_orostube_closed
   end
 
   def answer_with_details
@@ -98,7 +107,7 @@ class MessageResponder
                           *Prezzo:* #{item.price}0 €
                         }.gsub(/^[\s]+/, ""),
                         parse_mode: 'Markdown',
-                        answers: ['Indietro', "Ordina #{item.name}"]
+                        answers: ["\u{1f374} Menu", "Ordina #{item.name}"]
                         ).send
     else
       MessageSender.new(bot: bot,
@@ -142,12 +151,7 @@ class MessageResponder
     MessageSender.new(bot: bot,
                       chat: message.chat,
                       text: %{
-                        *Per vedere cosa c'è oggi:*
-                        ----
-                        /pizze per le pizze
-                        /insalate per le insalate
-                        /cucina per la cucina
-                        ----
+                        Usa il comando /start per iniziare (come ai vecchi tempi)
                       }.gsub(/^[\s]+/, ""),
                       parse_mode: 'Markdown'
                       ).send
